00bc39a31abc9c437aa875c75f6761632a2999d2
Jiawei Ren <jiaweirenMike@gmail.com>
2023-04-10 21:39:43 +1000

    create database

--- Data Governance/Create Database.sql
+++ Data Governance/Create Database.sql
@@ -1,7 +1,7 @@
-CREATE DATABASE songsDB ;
-USE songsDB; 
+CREATE DATABASE country_playist ;
+USE country_playist; 
 
-CREATE TABLE Albums (
+CREATE TABLE Album (
   album_id VARCHAR(255) NOT NULL PRIMARY KEY,
   album_name VARCHAR(255),
   album_type VARCHAR(255),
@@ -10,25 +10,21 @@ CREATE TABLE Albums (
   album_release_date DATE
 );
 
-CREATE TABLE Artists (
+CREATE TABLE Artist (
   artist_id VARCHAR(255) NOT NULL ,
   artist_name VARCHAR(255),
   artist_url VARCHAR(255),
   artist_genre VARCHAR(255),
-  artist_followers INT,
-  artist_popularity INT,
-  record_date DATE Not NUll,
-  PRIMARY KEY (artist_id, record_date)
+  PRIMARY KEY (artist_id)
 );
 
 
-CREATE TABLE Tracks (
+CREATE TABLE Track (
 	track_id VARCHAR(255)  NOT NULL,
 	album_id VARCHAR(255) NOT NULL,
 	artist_id VARCHAR(255) NOT NULL,
 	record_date DATE NOT NULL,
 	track_url VARCHAR(255),
-	track_popularity INT,
 	danceability FLOAT,
 	energy FLOAT,
 	key_ INT,
@@ -41,8 +37,43 @@ CREATE TABLE Tracks (
 	valence FLOAT,
 	tempo FLOAT,
 	duration_ms INT,
+    track_preview VARCHAR(255),
 	time_signature INT,
+    PRIMARY KEY (track_id),
+	FOREIGN KEY (album_id) REFERENCES Album(album_id),
+	FOREIGN KEY (artist_id) REFERENCES Artist(artist_id)
+);
+
+CREATE TABLE Track_Popularity (
+	record_date DATE,
+	track_id VARCHAR(255)  NOT NULL,
+    track_popularity INT,
     PRIMARY KEY (track_id, record_date),
-	FOREIGN KEY (album_id) REFERENCES Albums(album_id),
-	FOREIGN KEY (artist_id, record_date) REFERENCES Artists(artist_id, record_date)
+	FOREIGN KEY (track_id) REFERENCES Track(track_id)
+);
+
+CREATE TABLE artist_tracking (
+	record_date DATE,
+	artist_id VARCHAR(255)  NOT NULL,
+    artist_popularity INT,
+    artist_followers INT,
+    PRIMARY KEY (artist_id, record_date),
+	FOREIGN KEY (artist_id) REFERENCES Artist(artist_id)
+);
+
+CREATE TABLE Playlist (
+	playlist_id VARCHAR(255) NOT NULL,
+    country_code VARCHAR(255) NOT NULL,
+    country_name VARCHAR(255) ,
+    PRIMARY KEY (playlist_id)
+);
+
+CREATE TABLE Playlist_Tracks (
+	playlist_track_id VARCHAR(255) NOT NULL,
+    playlist_id VARCHAR(255) NOT NULL,
+    track_id VARCHAR(255) ,
+    record_date DATE,
+    PRIMARY KEY (playlist_id),
+    FOREIGN KEY (playlist_id) REFERENCES Playlist(playlist_id),
+    FOREIGN KEY (track_id) REFERENCES Track(track_id)
 );
